4141ad009c6d2bf33e8faa30bf13233c659ad0aa
MicheleSica <michele.sica@terradue.com>
2017-11-23 10:11:41 +0100

    ESAGEP-102 Inserted openjpeg2 in require restored the original package list modified install script

--- src/main/scripts/install.sh
+++ src/main/scripts/install.sh
@@ -2,10 +2,11 @@
 /opt/anaconda/bin/conda update conda -y
 
 /opt/anaconda/bin/conda install -y --file /tmp/.dependencies/packages.list
+/opt/anaconda/bin/conda create -n elibi libiconv=1.15=h63c8f33_5
 
 export PATH=/opt/anaconda/bin:$PATH
 export GDAL_DATA='/opt/anaconda/share/gdal'
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/anaconda/lib
+export LD_LIBRARY_PATH="/opt/anaconda/envs/elibi/lib:/opt/anaconda/lib:/opt/OTB-6.0.0/lib:$LD_LIBRARY_PATH"
 
 sed -i '26s/c++11/c++0x/' /opt/anaconda/lib/R/etc/Makeconf
 
