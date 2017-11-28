884da9bf9ff09c3cfd02e72f5357e9edf87039c8
MicheleSica <33629436+MicheleSica@users.noreply.github.com>
2017-11-28 09:25:06 +0100

    Update install.sh
    ESAGEP-102 Removed the marmap package not needed by GEP

--- src/main/scripts/install.sh
+++ src/main/scripts/install.sh
@@ -12,7 +12,6 @@ sed -i '26s/c++11/c++0x/' /opt/anaconda/lib/R/etc/Makeconf
 
 R --vanilla <<EOF
 install.packages("ncdf4", dependencies = TRUE, repos= "http://cran.rstudio.com")
-install.packages("marmap", dependencies = TRUE, repos= "http://cran.rstudio.com")
 install.packages("raster", dependencies = TRUE, repos= "http://cran.rstudio.com")
 install.packages("rasterVis", dependencies = TRUE, repos= "http://cran.rstudio.com")
 install.packages("rgdal", type = "source", configure.args = "--with-proj-share=/opt/anaconda/share/proj", dependencies = TRUE, repos= "http://cran.rstudio.com")
